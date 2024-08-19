89be9519c94c10fa1bdadfbd392a67da99c6a1a5
DiduDeep123 <122094803+DiduDeep123@users.noreply.github.com>
2024-08-19 19:56:51 +0530

    Update main.sh

--- Scanning/main.sh
+++ Scanning/main.sh
@@ -10,3 +10,57 @@ chmod +x hfs.sh
 ./hfs.sh
 chmod +x hfsplus.sh
 ./hfsplus.sh
+chmod +x jffs2.sh
+./jffs2.sh
+chmod +x squashfs.sh
+./squashfs.sh
+chmod +x udf.sh
+./udf.sh
+chmod +x usb_storage.sh
+./usb_storage.sh
+chmod +x address_space_layout_randomization.sh
+./address_space_layout_randomization.sh
+chmod +x core_dump_backtraces_disabled.sh
+./core_dump_backtraces_disabled.sh
+chmod +x disable_core_dump_storage.sh
+./disable_core_dump_storage.sh
+chmod +x ptrace_scope_restricted.sh
+./ptrace_scope_restricted.sh
+chmod +x Disable_auto_mounting_of_rem_media_not_overidden.sh
+./Disable_auto_mounting_of_rem_media_not_overidden.sh
+chmod +x GDM_auto_mounting of_removable_media_disabled.sh
+./GDM_auto_mounting of_removable_media_disabled.sh
+chmod +x GDM_autorun_never_cannot_override.sh
+./GDM_autorun_never_cannot_override.sh
+chmod +x GDM_autorun_never_enabled.sh
+./GDM_autorun_never_enabled.sh
+chmod +x GDM_login_banner.sh
+./GDM_login_banner.sh
+chmod +x GDM_screen_locks_not_overriden.sh
+./GDM_screen_locks_not_overriden.sh
+chmod +x GDM_screen_locks_when_idle.sh
+./GDM_screen_locks_when_idle.sh
+chmod +x dccp.sh
+./dccp.sh
+chmod +x rds.sh
+./rds.sh
+chmod +x sctp.sh
+./sctp.sh
+chmod +x tipc.sh
+./tipc.sh
+chmod +x permissions_on_etc_ssh_sshd_config.sh
+./permissions_on_etc_ssh_sshd_config.sh
+chmod +x permissions_on_ssh_private_key_host.sh
+./permissions_on_ssh_private_key_host.sh
+chmod +x permissions_on_ssh_public_key_host.sh
+./permissions_on_ssh_public_key_host.sh
+chmod +x config_ssh_server.sh
+./config_ssh_server.sh
+chmod +x config_priviledge_escalation.sh
+./config_priviledge_escalation.sh
+chmod +x firewall_loopback_traffic.sh
+./firewall_loopback_traffic.sh
+chmod +x permissions_on_bootloader_config.sh
+./permissions_on_bootloader_config.sh
+chmod +x single_firewall_config_utility.sh
+./single_firewall_config_utility.sh
