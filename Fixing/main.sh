82e2bc4c6cf6c9eb4cafe504a24a5f3877891d31
DiduDeep123 <122094803+DiduDeep123@users.noreply.github.com>
2024-08-20 09:34:45 +0530

    Update main.sh

--- Fixing/main.sh
+++ Fixing/main.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-chmod +x fixing_script_1.sh fix_cramfs.sh fix_freevxfs.sh fix_hfs.sh fix_hfsplus.sh fix_jfss2.sh fix_squashfs.sh fix_udf.sh fix_usb_storage.sh fix_dccp.sh
+chmod +x fixing_script_1.sh fix_cramfs.sh fix_freevxfs.sh fix_hfs.sh fix_hfsplus.sh fix_jfss2.sh fix_squashfs.sh \
+fix_udf.sh fix_usb_storage.sh fix_dccp.sh fix_rds.sh fix_sctp.sh fix_tipc.sh
 
 ./fixing_script_1.sh
 ./fix_cramfs.sh
