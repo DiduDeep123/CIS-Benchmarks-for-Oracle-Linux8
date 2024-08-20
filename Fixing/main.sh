484b86d1cd1eaf1d12abca96fda98f7ba13735f4
DiduDeep123 <122094803+DiduDeep123@users.noreply.github.com>
2024-08-20 10:57:45 +0530

    Update main.sh

--- Fixing/main.sh
+++ Fixing/main.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
 
 chmod +x fixing_script_1.sh fix_cramfs.sh fix_freevxfs.sh fix_hfs.sh fix_hfsplus.sh fix_jfss2.sh fix_squashfs.sh \
-fix_udf.sh fix_usb_storage.sh fix_dccp.sh fix_rds.sh fix_sctp.sh fix_tipc.sh disable_packet_redirect_sending.sh \
-enable_reverse_path_filtering.sh enable_tcp_syn_cookies.sh ensure_ip_forwarding_disabled.sh ignore_bogus_icmp_responses.sh \
+fix_udf.sh fix_usb_storage.sh fix_dccp.sh fix_rds.sh fix_sctp.sh fix_tipc.sh disable_packet_redirect_sending.sh 
+
+chmod +x enable_reverse_path_filtering.sh enable_tcp_syn_cookies.sh ensure_ip_forwarding_disabled.sh ignore_bogus_icmp_responses.sh \
 ignore_broadcast_icmp_req.sh log_suspicious_packets.sh reject_icmp_redirects.sh reject_ipv6_router_advertisements.sh \
 reject_source_routed_packets.sh fix_journald.sh
+
+chmod +x 
 
 
 
