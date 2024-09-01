aebc9ac3b81784950b1abd39b69bf10c6a5d8e55
DiduDeep123 <122094803+DiduDeep123@users.noreply.github.com>
2024-09-01 17:38:16 +0530

    Update fix_GDM_disable_user_list.sh

--- Fixing/fix_GDM_disable_user_list.sh
+++ Fixing/fix_GDM_disable_user_list.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/bash
 
+LOG_FILE="/var/log/hardening_fix.log"
+RESULT_FILE="/var/tmp/scan_results.txt"
+NEW_LOG_FILE="/var/log/user_select.log"
+
+# Logging function
+log_message() {
+    echo "$(date +"%Y-%m-%d %H:%M:%S") $1" | tee -a $LOG_FILE
+}
+
+#User opted not to apply remediation function
+select_no() {
+	log_message "User opted not to apply remediation."
+	echo "$(date +"%Y-%m-%d %H:%M:%S") $1" | tee -a $NEW_LOG_FILE
+}
+
+grep -q "GDM LOGIN BANNER:NOT CONFIGURED" "$RESULT_FILE" && grep -q "GDM:INSTALLED" "$RESULT_FILE"; then
+	read -p "Do you want to configure GDM login banner? (y/n)" answer
+	if [[ answer = [Yy] ]]; then
+
  l_gdmprofile="gdm"
  if [ ! -f "/etc/dconf/profile/$l_gdmprofile" ]; then
  echo "Creating profile \"$l_gdmprofile\""
