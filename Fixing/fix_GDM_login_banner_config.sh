8a3b61e72ff2d43d5f34d039727e292b34054ab3
DiduDeep123 <122094803+DiduDeep123@users.noreply.github.com>
2024-09-01 17:10:41 +0530

    Update fix_GDM_login_banner_config.sh

--- Fixing/fix_GDM_login_banner_config.sh
+++ Fixing/fix_GDM_login_banner_config.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/bash
 
+
+LOG_FILE="/var/log/hardening_fix.log"
+RESULT_FILE="/var/tmp/scan_results.txt"
+NEW_LOG_FILE="/var/log/user_select.log"
+
+# Logging function
+log_message() {
+    echo "$(date +"%Y-%m-%d %H:%M:%S") $1" | tee -a $LOG_FILE
+}
+
+#User opted not to apply remediation function
+select_no() {
+	log_message "User opted not to apply remediation."
+	echo "$(date +"%Y-%m-%d %H:%M:%S") $1" | tee -a $NEW_LOG_FILE
+}
+
  l_pkgoutput=""
  if command -v dpkg-query > /dev/null 2>&1; then
  l_pq="dpkg-query -W"
