89d93e69ddc35804164fb0b57cf502054e268ddc
DiduDeep123 <122094803+DiduDeep123@users.noreply.github.com>
2024-09-01 16:07:09 +0530

    Update fix_permissions_on_bootloader_config.sh

--- Fixing/fix_permissions_on_bootloader_config.sh
+++ Fixing/fix_permissions_on_bootloader_config.sh
@@ -1,4 +1,22 @@
-#!usr/bin/bash
+#!/usr/bin/bash
+
+
+LOG_FILE="/var/log/hardening_fix.log"
+RESULT_FILE="/var/tmp/scan_results.txt"
+NEW_LOG_FILE="/var/log/user_select.log"
+
+# Logging function
+log_message() {
+    echo "$(date +"%Y-%m-%d %H:%M:%S") $1" | tee -a $LOG_FILE
+}
+
+#User opted not to apply remediation function
+select_no() {
+	log_message "User opted not to apply remediation."
+	echo "$(date +"%Y-%m-%d %H:%M:%S") $1" | tee -a $NEW_LOG_FILE
+}
+
+
 
 # Function to update UEFI settings in /etc/fstab
 update_uefi_fstab() {
