593e11fc6d4159b8852d0f848439fce1243333f1
DiduDeep123 <122094803+DiduDeep123@users.noreply.github.com>
2024-09-07 08:40:44 +0530

    Update fix_jfss2.sh

--- Fixing/fix_jfss2.sh
+++ Fixing/fix_jfss2.sh
@@ -13,7 +13,7 @@ log_message() {
 if grep -q "JFFS2 CHECK: FAILED" $RESULT_FILE; then
 
 read -p "Do you want to remove jffs2 module? (y/n)" answer
-if [[ answer = [Yy] ]]; then
+if [[ $answer = [Yy] ]]; then
 log_message "Starting remediation of jffs2 module..."
  l_mname="jffs2" # set module name
  l_mtype="fs" # set module type
