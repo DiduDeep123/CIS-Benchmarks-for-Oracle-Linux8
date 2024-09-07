4bbd5fb34c03a548f010292ffb3f22786cc07880
DiduDeep123 <122094803+DiduDeep123@users.noreply.github.com>
2024-09-07 10:19:46 +0530

    Update jffs2.sh

--- Scanning/jffs2.sh
+++ Scanning/jffs2.sh
@@ -8,6 +8,8 @@ log_message() {
     echo "$(date +"%Y-%m-%d %H:%M:%S") $1" | tee -a $LOG_FILE	  #output to both screen and log file
 }
 
+log_message "Checking if jffs2 module is not available..."
+
  l_output="" l_output2="" l_output3="" l_dl="" # Unset output variables
  l_mname="jffs2" # set module name
  l_mtype="fs" # set module type
