3b2077a9c7512f7f3b6c486da8fb397ddb561216
DiduDeep123 <122094803+DiduDeep123@users.noreply.github.com>
2024-09-07 11:29:25 +0530

    Update scan_script_1.sh

--- Scanning/scan_script_1.sh
+++ Scanning/scan_script_1.sh
@@ -19,8 +19,6 @@ check_crond_status() {
     enabled_status=$(systemctl is-enabled crond 2>/dev/null)	#direct any error messages so that it does not clutter the output
     active_status=$(systemctl is-active crond 2>/dev/null)
 
-    log_message "Checking crond status..."
-
     if [[ $enabled_status == "enabled" && $active_status == "active" ]]; then
         echo "crond:enabled:active" > $RESULT_FILE
         log_message "crond is enabled and active."
@@ -41,8 +39,6 @@ check_crontab_permissions() {
     desired_access_permissions="600"
     desired_user="root"
     desired_group="root"
-    
-    log_message "Checking crontab permissions..."
     
     if [[ "$current_access_permissions" == "$desired_access_permissions" && "$current_user" == "$desired_user" && "$current_group" == "$desired_group" ]]; then
         echo "crontab:correct_permissions" >> $RESULT_FILE
@@ -65,8 +61,6 @@ check_cron_hourly_permissions() {
     desired_user="root"
     desired_group="root"
     
-    log_message "Checking cron.hourly permissions..."
-    
     if [[ "$current_access_permissions" == "$desired_access_permissions" && "$current_user" == "$desired_user" && "$current_group" == "$desired_group" ]]; then
         echo "cron.hourly:correct_permissions" >> $RESULT_FILE
         log_message ""$CRON_HOURLY_DIR" permissions are correctly configured."
@@ -88,7 +82,6 @@ check_cron_daily_permissions() {
     desired_user="root"
     desired_group="root"
     
-    log_message "Checking cron.daily permissions..."
     
     if [[ "$current_access_permissions" == "$desired_access_permissions" && "$current_user" == "$desired_user" && "$current_group" == "$desired_group" ]]; then
         echo "cron.daily:correct_permissions" >> $RESULT_FILE
@@ -111,7 +104,7 @@ check_cron_weekly_permissions() {
     desired_user="root"
     desired_group="root"
     
-    log_message "Checking cron.weekly permissions..."
+    
     
     if [[ "$current_access_permissions" == "$desired_access_permissions" && "$current_user" == "$desired_user" && "$current_group" == "$desired_group" ]]; then
         echo "cron.weekly:correct_permissions" >> $RESULT_FILE
@@ -134,7 +127,7 @@ check_cron_monthly_permissions() {
     desired_user="root"
     desired_group="root"
     
-    log_message "Checking cron.monthly permissions..."
+    
     
     if [[ "$current_access_permissions" == "$desired_access_permissions" && "$current_user" == "$desired_user" && "$current_group" == "$desired_group" ]]; then
         echo "cron.monthly:correct_permissions" >> $RESULT_FILE
@@ -157,7 +150,7 @@ check_crond_permissions() {
     desired_user="root"
     desired_group="root"
     
-    log_message "Checking cron.d permissions..."
+   
     
     if [[ "$current_access_permissions" == "$desired_access_permissions" && "$current_user" == "$desired_user" && "$current_group" == "$desired_group" ]]; then
         echo "cron.d:correct_permissions" >> $RESULT_FILE
@@ -167,6 +160,73 @@ check_crond_permissions() {
         log_message ""$CROND" permissions need to be changed."
     fi
 }
+
+# Check if crontab is restricted for authorized users
+
+check_file() {
+    local file_path=$1
+    local expected_mode=$2
+
+    if [ -e "$file_path" ]; then
+        echo "Checking $file_path..."
+
+        # Get file properties
+        file_stat=$(stat -Lc 'Access: (%a/%A) Owner: (%U) Group: (%G)' "$file_path")
+        echo "$file_stat"
+
+        # Extract mode, owner, and group from stat output
+        local mode=$(echo "$file_stat" | awk -F' ' '{print $2}' | cut -d'/' -f1)
+        local owner=$(echo "$file_stat" | awk -F' ' '{print $4}')
+        local group=$(echo "$file_stat" | awk -F' ' '{print $6}')
+
+        # Check if mode is 0640 or more restrictive
+        if [ "$mode" -le "$expected_mode" ] && [ "$owner" = "root" ] && [ "$group" = "root" ]; then
+            echo "$file_path is configured correctly."
+	    echo "$file_path:correctly configured" >> $RESULT_FILE
+        else
+            echo "$file_path does not meet the required configuration."
+	    echo "$file_path:incorrectly configured" >> $RESULT_FILE
+            echo "Expected mode: 0640 or more restrictive, Owner: root, Group: root."
+        fi
+    else
+        echo "$file_path does not exist."
+	echo "$file_path:DOES NOT EXIST" >> $RESULT_FILE
+    fi
+}
+
+# Function to check cron configuration
+check_cron_configuration() {
+    echo "Checking /etc/cron.allow:"
+    check_file "/etc/cron.allow" 640
+
+    echo "Checking /etc/cron.deny:"
+    if [ -e "/etc/cron.deny" ]; then
+        check_file "/etc/cron.deny" 640
+    else
+        echo "/etc/cron.deny does not exist, which is acceptable."
+    fi
+}
+
+
+if command -v crond > /dev/null 2>&1; then
+    log_message "Checking crond status..."
+    check_crond_status
+    log_message "Checking crontab permissions..."
+    check_crontab_permissions
+    log_message "Checking cron.hourly permissions..."
+    check_cron_hourly_permissions
+    log_message "Checking cron.daily permissions..."
+    check_cron_daily_permissions
+    log_message "Checking cron.weekly permissions..."
+    check_cron_weekly_permissions
+    log_message "Checking cron.monthly permissions..."
+    check_cron_monthly_permissions
+    log_message "Checking cron.d permissions..."
+    check_crond_permissions
+    log_message "Checking if crontab is restricted for authorized users..."
+    check_cron_configuration
+fi
+
 
 
 # Check if /tmp is mounted
@@ -1440,15 +1500,8 @@ check_nftables_base_chains_policy() {
 
 
 
-log_message "Starting scan..."
 
-check_crond_status
-check_crontab_permissions
-check_cron_hourly_permissions
-check_cron_daily_permissions
-check_cron_weekly_permissions
-check_cron_monthly_permissions
-check_crond_permissions
+
 check_tmp_mount
 check_systemd_status_tmp
 check_fstab_for_tmp
