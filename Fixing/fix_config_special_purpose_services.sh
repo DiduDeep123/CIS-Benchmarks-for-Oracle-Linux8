83f25c02a08d005a219e6f77fa8af770fa73c59b
DiduDeep123 <122094803+DiduDeep123@users.noreply.github.com>
2024-09-16 09:57:04 +0530

    Update fix_config_special_purpose_services.sh

--- Fixing/fix_config_special_purpose_services.sh
+++ Fixing/fix_config_special_purpose_services.sh
@@ -386,63 +386,7 @@ fi
 
 }
 
-#fix ftp
-fix_ftp(){
-if grep -q "ftp:INSTALLED" $RESULT_FILE; then
-	read -p "Do you want to remove ftp? (y/n)" answer
-	if [[ answer = [Yy] ]]; then
-		dnf remove ftp
 
-	fi
-fi
-
-}
-
-#fix ldap
-fix_ldap(){
-if grep -q "openldap-clients:INSTALLED" $RESULT_FILE; then
-	read -p "Do you want to remove ldap? (y/n)" answer
-	if [[ answer = [Yy] ]]; then
-		dnf remove openldap-clients
-
-	fi
-fi
-
-}
-
-#fix nis
-fix_nis() {
-if grep -q "openldap-clients:INSTALLED" $RESULT_FILE; then
-	read -p "Do you want to remove ldap? (y/n)" answer
-	if [[ answer = [Yy] ]]; then
-		dnf remove openldap-clients
-
-	fi
-fi
-}
-
-#fix telnet
-fix_telnet() {
-if grep -q "telnet:INSTALLED" $RESULT_FILE; then
-	read -p "Do you want to remove telnet? (y/n)" answer
-	if [[ answer = [Yy] ]]; then
-		dnf remove telnet
-
-	fi
-fi
-}
-
-#fix tftp
-fix_tftp() {
-if grep -q "tftp:INSTALLED" $RESULT_FILE; then
-	read -p "Do you want to remove tftp? (y/n)" answer
-	if [[ answer = [Yy] ]]; then
-		dnf remove tftp
-
-	fi
-fi
-
-}
 
 #fix bluez
 fix_bluez() {
@@ -486,10 +430,5 @@ fix_squid
 fix_nginx
 fix_xinetd
 fix_xorg_x11_server_common
-fix_ftp
-fix_ldap
-fix_nis
-fix_telnet
-fix_tftp
 fix_bluez
 
