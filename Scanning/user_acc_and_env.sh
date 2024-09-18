f1c6ab264cdf480280ad2448a1c01f4ab20bc5b6
DiduDeep123 <122094803+DiduDeep123@users.noreply.github.com>
2024-09-18 19:49:37 +0530

    Update user_acc_and_env.sh

--- Scanning/user_acc_and_env.sh
+++ Scanning/user_acc_and_env.sh
@@ -14,10 +14,10 @@ check_libuser_conf() {
 
   # Check if crypt_style is set to sha512 or yescrypt
   if grep -Pi -- '^\s*crypt_style\s*=\s*(sha512|yescrypt)\b' /etc/libuser.conf >/dev/null; then
-    echo "LIBUSER CONFIG:OK" >> "$RESULT_FILE"
+    echo "CRYPT STYLE:STRONG" >> "$RESULT_FILE"
     result="crypt_style is set to sha512 or yescrypt in /etc/libuser.conf. Configuration is compliant."
   else
-    echo "LIBUSER CONFIG:REQUIRES CHANGE" >> "$RESULT_FILE"
+    echo "CRYPT STYLE:WEAK" >> "$RESULT_FILE"
     result="crypt_style is not set to sha512 or yescrypt in /etc/libuser.conf."
   fi
 
@@ -30,10 +30,10 @@ check_login_defs() {
 
   # Check if ENCRYPT_METHOD is set to SHA512 or yescrypt
   if grep -Pi -- '^\s*ENCRYPT_METHOD\s+(SHA512|yescrypt)\b' /etc/login.defs >/dev/null; then
-    echo "LOGIN DEFS CONFIG:OK" >> "$RESULT_FILE"
+    echo "ENCRYPT_METHOD:STRONG" >> "$RESULT_FILE"
     result="ENCRYPT_METHOD is set to SHA512 or yescrypt in /etc/login.defs. Configuration is compliant."
   else
-    echo "LOGIN DEFS CONFIG:REQUIRES CHANGE" >> "$RESULT_FILE"
+    echo "ENCRYPT_METHOD:WEAK" >> "$RESULT_FILE"
     result="ENCRYPT_METHOD is not set to SHA512 or yescrypt in /etc/login.defs."
   fi
 
@@ -46,15 +46,80 @@ check_pass_max_days() {
 
   # Check if PASS_MAX_DAYS is set to 365 or less
   if grep -E '^\s*PASS_MAX_DAYS\s+[0-9]+' /etc/login.defs | awk '{print $2}' | awk -v max_days=365 '$1 > max_days' >/dev/null; then
-    echo "LOGIN DEFS CONFIG:REQUIRES CHANGE" >> "$RESULT_FILE"
+    echo "PASS_MAX_DAYS:REQUIRES CHANGE" >> "$RESULT_FILE"
     result="PASS_MAX_DAYS is set to more than 365 days in /etc/login.defs. Configuration requires change."
   else
-    echo "LOGIN DEFS CONFIG:OK" >> "$RESULT_FILE"
+    echo "PASS_MAX_DAYS:OK" >> "$RESULT_FILE"
     result="PASS_MAX_DAYS is set to 365 days or less in /etc/login.defs. Configuration is compliant."
   fi
 
   log_message "$result"
 }
+
+
+check_users_pass_max_days() {
+    log_message "Checking /etc/shadow for users' PASS_MAX_DAYS values..."
+
+    local non_compliant_found=false
+
+    users_pass_max_days=$(grep -E '^[^:]+:[^!*]' /etc/shadow | cut -d: -f1,5)
+
+    while IFS=: read -r user pass_max_days; do
+        # If PASS_MAX_DAYS is empty default to unlimited (0)
+        if [ -z "$pass_max_days" ]; then
+            pass_max_days="0"
+        fi
+
+        # Check if PASS_MAX_DAYS is more than 365
+        if [ "$pass_max_days" -gt 365 ]; then
+            echo "User: $user, PASS_MAX_DAYS: $pass_max_days - REQUIRES CHANGE" >> "$RESULT_FILE"
+            log_message "User $user has PASS_MAX_DAYS set to $pass_max_days, which exceeds 365."
+            echo "PASS_MAX_DAYS_ETC_SHADOW:REQUIRES CHANGE" >> "$RESULT_FILE"
+            non_compliant_found=true
+        else
+            echo "User: $user, PASS_MAX_DAYS: $pass_max_days - OK" >> "$RESULT_FILE"
+            log_message "User $user has PASS_MAX_DAYS set to $pass_max_days, which is compliant."
+            echo "PASS_MAX_DAYS_ETC_SHADOW:OK" >> "$RESULT_FILE"
+        fi
+    done <<< "$users_pass_max_days"
+
+ 
+    if $non_compliant_found; then
+        log_message "Some users have PASS_MAX_DAYS exceeding 365 days. Review required."
+    else
+        log_message "All users have PASS_MAX_DAYS within the policy (<= 365 days)."
+    fi
+}
+
+
+check_pass_warn_age() {
+    local config_file="/etc/login.defs"
+    local min_warn_age=7
+
+    # Check if the file exists
+    if [ ! -f "$config_file" ]; then
+        echo "Error: $config_file not found!"
+        return 1
+    fi
+
+    # Extract the PASS_WARN_AGE value from /etc/login.defs
+    pass_warn_age=$(grep -E '^\s*PASS_WARN_AGE\s+' "$config_file" | awk '{print $2}')
+
+    # Check if PASS_WARN_AGE is set and whether it meets the minimum required value
+    if [[ -n "$pass_warn_age" ]]; then
+        if [[ "$pass_warn_age" -ge $min_warn_age ]]; then
+            log_message "PASS_WARN_AGE is set to $pass_warn_age days and conforms to site policy."
+            echo "PASS_WARN_AGE:OK" >> "$RESULT_FILE"
+        else
+             log_message "PASS_WARN_AGE is set to $pass_warn_age days and does not conform to site policy. Requires change."
+              echo "PASS_WARN_AGE:REQUIRES CHANGE"
+        fi
+    else
+        log_message "PASS_WARN_AGE is not set in $config_file. Configuration requires change."
+         echo "PASS_WARN_AGE:REQUIRES CHANGE"
+    fi
+}
+
 
 # Function to check users for PASS_WARN_AGE
 check_user_warn_age() {
@@ -63,16 +128,16 @@ check_user_warn_age() {
   # Check if any users have PASS_WARN_AGE set to less than 7 days
   awk -F: '/^[^:\n\r]+:[^!\*xX\n\r]/ {print $1 ":" $6}' /etc/shadow | while IFS=: read user warn_age; do
     if [[ "$warn_age" -lt 7 ]]; then
-      echo "USER CONFIG:REQUIRES CHANGE - $user has PASS_WARN_AGE set to $warn_age which is less than 7 days." >> "$RESULT_FILE"
+      echo "PASS_WARN_AGE_ETC_SHADOW:REQUIRES CHANGE - $user has PASS_WARN_AGE set to $warn_age which is less than 7 days." >> "$RESULT_FILE"
     fi
   done
 
   # Check if all users have PASS_WARN_AGE set to 7 or more days
   if awk -F: '/^[^:\n\r]+:[^!\*xX\n\r]/ {print $6}' /etc/shadow | awk -v min_warn=7 '$1 < min_warn' | read -r; then
-    echo "USER CONFIG:REQUIRES CHANGE" >> "$RESULT_FILE"
+    echo "PASS_WARN_AGE_ETC_SHADOW:REQUIRES CHANGE" >> "$RESULT_FILE"
     result="Some users have PASS_WARN_AGE set to less than 7 days."
   else
-    echo "USER CONFIG:OK" >> "$RESULT_FILE"
+    echo "PASS_WARN_AGE_ETC_SHADOW:OK" >> "$RESULT_FILE"
     result="All users have PASS_WARN_AGE set to 7 days or more."
   fi
 
@@ -85,10 +150,10 @@ check_inactive_default() {
 
   # Check the INACTIVE value set for new users
   if useradd -D | grep -E 'INACTIVE\s*=\s*[0-9]+' | awk -F= '{print $2}' | awk -v max_inactive=30 '$1 > max_inactive' >/dev/null; then
-    echo "DEFAULT CONFIG:REQUIRES CHANGE" >> "$RESULT_FILE"
+    echo "INACTIVE:REQUIRES CHANGE" >> "$RESULT_FILE"
     result="INACTIVE is set to more than 30 days in default user settings. Configuration requires change."
   else
-    echo "DEFAULT CONFIG:OK" >> "$RESULT_FILE"
+    echo "INACTIVE:OK" >> "$RESULT_FILE"
     result="INACTIVE is set to 30 days or less in default user settings. Configuration is compliant."
   fi
 
@@ -102,22 +167,34 @@ check_user_inactive() {
   # Check if any users have INACTIVE set to more than 30 days
   awk -F: '/^[^#:]+:[^!\*:]*:[^:]*:[^:]*:[^:]*:[^:]*:(\s*|-1|3[1-9]|[4-9][0-9]|[1-9][0-9][0-9]+):[^:]*:[^:]*\s*$/ {print $1":"$7}' /etc/shadow | while IFS=: read user inactive; do
     if [[ "$inactive" -gt 30 ]]; then
-      echo "USER CONFIG:REQUIRES CHANGE - $user has INACTIVE set to $inactive which is more than 30 days." >> "$RESULT_FILE"
+      echo "INACTIVE_ETC_SHADOW:REQUIRES CHANGE - $user has INACTIVE set to $inactive which is more than 30 days." >> "$RESULT_FILE"
     fi
   done
 
   # Check if all users have INACTIVE set to 30 days or less
   if awk -F: '/^[^#:]+:[^!\*:]*:[^:]*:[^:]*:[^:]*:[^:]*:(\s*|-1|3[1-9]|[4-9][0-9]|[1-9][0-9][0-9]+):[^:]*:[^:]*\s*$/ {print $7}' /etc/shadow | awk -v max_inactive=30 '$1 > max_inactive' | read -r; then
-    echo "USER CONFIG:REQUIRES CHANGE" >> "$RESULT_FILE"
+    echo "INACTIVE_ETC_SHADOW:REQUIRES CHANGE" >> "$RESULT_FILE"
     result="Some users have INACTIVE set to more than 30 days."
   else
-    echo "USER CONFIG:OK" >> "$RESULT_FILE"
+    echo "INACTIVE_ETC_SHADOW:OK" >> "$RESULT_FILE"
     result="All users have INACTIVE set to 30 days or less."
   fi
 
   log_message "$result"
 }
 
+verify_password_change_dates() {
+    log_message "Ensuring all users last password change date is in the past..."
+    while IFS= read -r l_user; do
+        
+        l_change=$(date -d "$(chage --list "$l_user" | grep '^Last password change' | cut -d: -f2 | grep -v 'never$')" +%s)
+        
+        
+        if [[ "$l_change" -gt "$(date +%s)" ]]; then
+          log_message "User: \"$l_user\" last password change was \"$(chage --list "$l_user" | grep '^Last password change' | cut -d: -f2)\""
+        fi
+    done < <(awk -F: '/^[^:\n\r]+:[^!*xX\n\r]/{print $1}' /etc/shadow)
+}
 
 # Function to check root's primary GID
 check_root_gid() {
@@ -137,6 +214,11 @@ check_root_gid() {
 
   log_message "$result"
 }
+
+
+
+
+
 
 # Function to check umask settings
 check_umask() {
@@ -183,10 +265,10 @@ check_nologin_in_shells() {
 
   # Search for '/nologin' in /etc/shells
   if grep -P '/nologin\b' /etc/shells >/dev/null; then
-    log_message "SHELL CONFIG:REQUIRES CHANGE - /nologin is listed in /etc/shells"
+    echo "NO LOGIN CONFIG:REQUIRES CHANGE" >> "$RESULT_FILE"
     result="/nologin is listed in /etc/shells. Configuration requires change."
   else
-    log_message "SHELL CONFIG:OK - /nologin is not listed in /etc/shells"
+    echo "NO LOGIN CONFIG:OK" >> "$RESULT_FILE"
     result="/nologin is not listed in /etc/shells. Configuration is compliant."
   fi
 
@@ -194,12 +276,51 @@ check_nologin_in_shells() {
 }
 
 
+
+# Function to check and report TMOUT configuration
+check_tmout_configuration() {
+    local output1=""
+    local output2=""
+    local BRC="/etc/bashrc"
+
+    # Check if /etc/bashrc exists and set BRC
+    [ -f "$BRC" ] && BRC="/etc/bashrc"
+
+    # Check TMOUT settings in specified files
+    for f in "$BRC" /etc/profile /etc/profile.d/*.sh; do
+        if grep -Pq '^\s*([^#]+\s+)?TMOUT=(900|[1-8][0-9][0-9]|[1-9][0-9]|[1-9])\b' "$f" &&
+           grep -Pq '^\s*([^#]+;\s*)?readonly\s+TMOUT(\s+|\s*;|\s*$|=(900|[1-8][0-9][0-9]|[1-9][0-9]|[1-9]))\b' "$f" &&
+           grep -Pq '^\s*([^#]+;\s*)?export\s+TMOUT(\s+|\s*;|\s*$|=(900|[1-8][0-9][0-9]|[1-9][0-9]|[1-9]))\b' "$f"; then
+            output1="$f"
+        fi
+    done
+
+    # Check for incorrect TMOUT settings
+    grep -Pq '^\s*([^#]+\s+)?TMOUT=(9[0-9][1-9]|9[1-9][0-9]|0+|[1-9]\d{3,})\b' /etc/profile /etc/profile.d/*.sh "$BRC" &&
+    output2=$(grep -Ps '^\s*([^#]+\s+)?TMOUT=(9[0-9][1-9]|9[1-9][0-9]|0+|[1-9]\d{3,})\b' /etc/profile /etc/profile.d/*.sh "$BRC")
+
+    # Report results
+    if [ -n "$output1" ] && [ -z "$output2" ]; then
+        echo -e "\nPASSED\n\nTMOUT is configured correctly in: \"$output1\"\n"
+    else
+        [ -z "$output1" ] && echo -e "\nFAILED\n\nTMOUT is not configured correctly\n"
+        [ -n "$output2" ] && echo -e "\nFAILED\n\nTMOUT is incorrectly configured in: \"$output2\"\n"
+    fi
+}
+
+
+
+
 check_libuser_conf
 check_login_defs
 check_pass_max_days
+check_users_pass_max_days
 check_user_inactive
+check_pass_warn_age
 check_user_warn_age
 check_inactive_default
+verify_password_change_dates
 check_root_gid
 check_root_password_set
 check_nologin_in_shells
+check_tmout_configuration
